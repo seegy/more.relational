1cb36ee5caa6ee8a304e2d0a852a386d50324f84
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-05-26 23:17:35 +0200

    insert big tests

--- src/relation/tests.clj
+++ src/relation/tests.clj
@@ -8,6 +8,9 @@
 
 (def rs (rel #{{:id 1 :sid 1 :quantity 200} {:id 1 :sid 2 :quantity 2} {:id 2 :sid 3 :quantity 100} {:id 2 :sid 2 :quantity 1}}))
 
+(seq r)
+
+
 (rename r {:id :di})
 
 (rename* r #"(.+)" "prefix-$1")
@@ -82,21 +85,56 @@
  ;; ######################## BIG TEST ############################
 
 
- (def employees-data (set (first (map #(read-string (str "[" % "]"))  (let [rdr (clojure.java.io/reader "resources/employees.clj")]
-          (line-seq rdr))))))
+ (def employees-data (set (read-string  (str "[" (slurp  "resources/employees.clj" ) "]" ))))
+ (count employees-data)
 
-
+ (def employees-data (take 10000 employees-data))
+ (print "read employees")
  (time (def employees (rel [:emp_no :birth_date :first_name :last_name :gender :hire_date] employees-data))); auf MAC: 65.3 ms
 
- (def salaries-data (set (first (map #(read-string (str "[" % "]"))  (let [rdr (clojure.java.io/reader "resources/salaries.clj")]
-          (line-seq rdr))))))
+ (def salaries-data (set (read-string  (str "[" (slurp  "resources/salaries.clj" ) "]" ))))
+ (count salaries-data)
 
 
+ (def salaries-data (take 5000 salaries-data))
+ (print "read salaries")
  (time (def salaries (rel [:emp_no :salary :from_date :to_date] salaries-data))) ; auf MAC: 80.5ms
 
-
+(print "join")
  (time (def es (join employees salaries))) ; auf MAC 272281.8ms
+(count es)
 
 
 
 
+
+
+(def employees-data (set (read-string  (str "[" (slurp  "resources/employees.clj" ) "]" ))))
+ (count employees-data)
+
+ (print "read employees")
+ (time (def employees (rel [:emp_no :birth_date :first_name :last_name :gender :hire_date] employees-data))); auf MAC: 65.3 ms
+
+
+ (println "bereichssuche")
+;;  Bereichssuchen
+(time (restrict employees (relfn [r] (= (:gender r) "M"))))
+(time (restrict employees (relfn [r] (and (= (:first_name r) "Arvin") (= (:gender r) "M")))))
+(time (-> employees (restrict (relfn [r] (and (= (:first_name r) "Arvin") (= (:gender r) "M")))) (project [:emp_no :first_name])))
+(time (restrict employees (relfn [r] (= (:birth_date r) "1964-10-29"))))
+
+
+
+ (println "punktsuche")
+;; Punktsuchen
+(time (restrict employees (relfn [r] (= (:emp_no r) 429270))))
+(time (restrict employees (relfn [r] (= (:birth_date r) "1964-10-29"))))
+(time (restrict employees (relfn [r] (and (= (:gender r) "M") (= (:birth_date r) "1962-07-15") (= (:last_name r) "Perl") (= (:first_name r) "Arvin") (= (:gender r) "M")))))
+
+ (println "andere operationen")
+;; Andere Operationen
+(time (rename employees {:emp_no :id}))
+(time (union employees employees))
+(time (group employees {:dates #{:birth_date :hire_date}}))
+(time (summarize employees #{:emp_no} {:scount (relfn [r] (count r))}))
+(time (summarize employees #{} {:quantitysum (relfn [r] (reduce + (:emp_no r)))}))

b2c8b07bce98c4540633578416032f70c5642ec7
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-09-09 12:50:03 +0200

    refactored table code

--- src/relation/transrelational/table.clj
+++ src/relation/transrelational/table.clj
@@ -1,8 +1,6 @@
 (ns relation.transrelational.table)
 
 
-
-
 (deftype Transrelation [keyorder fvt rrt]
    clojure.lang.Counted
     (count [this]
@@ -12,18 +10,21 @@
 
 
 
-(defn keyorder [tr]
-  (.keyorder tr))
+(defn keyorder
+  [tr]
+    (.keyorder tr))
 
 
 
-(defn fieldValues [tr]
-  (.fvt tr))
+(defn fieldValues
+  [tr]
+    (.fvt tr))
 
 
 
-(defn recordReconst [tr]
-  (.rrt tr))
+(defn recordReconst
+  ([tr]
+   (.rrt tr)))
 
 
 
@@ -36,11 +37,14 @@
 
 
 
+
 (defn fieldValueOf
   ""
   [tr row column]
-  (let [columName (if (keyword? column) column (get (keyorder tr) column))]
-     (:value (nth (get (fieldValues tr) columName) row))))
+  (let [columnName (if (keyword? column) column (get (keyorder tr) column))
+        xf (comp #(:value %) #(nth % row) #(get % columnName))]
+     (xf (fieldValues tr))))
+
 
 
 
@@ -48,22 +52,26 @@
 (defn tr
   ""
   ([table]
-  (let [keyorder  (vec (keys (first table)))
-        recordtuples (map (fn[record](reduce (fn[m [k v]]
-                                               (assoc m k [v (.indexOf table record)]))
-                                             {} record)) table)
-        permutation (reduce (fn[ m head]
-                              (assoc m head (sort-by first
-                                                     (map (fn[record](get record head))
-                                                          recordtuples))))
-                            {} keyorder)
-        fvt (reduce (fn[m [k v]](assoc m k
-                                  (let [ values (map (fn[cell] (first cell)) v)]
-                                    (distinct (map (fn[v] {:value v :from (.indexOf values v) :to (.lastIndexOf values v)}) values))))) {} permutation)
+  (let [table (distinct table)
+        keyorder  (into [] (keys (first table)))
+        recordtuples (map (fn[record] (into {}
+                                            (map (fn[[k v]] [k [v (.indexOf table record)]]))
+                                            record)) table)
+        permutation (into {} (map (fn[head] [head (sort-by first
+                                                     (map #(get % head)
+                                                          recordtuples))])) keyorder)
 
-        rrt (let [perm  (reduce (fn[m [k v]](assoc m k  (map second v))) {} permutation)
-                  zigzagTo (fn [[a b]](vec (map (fn[recnr] (.indexOf b recnr)) a)))
-                  fromPerm (vec (map (fn[x](get perm x)) keyorder))
+        fvt (into {} (map (fn[[k v]] [ k (let [ values (map first v)]
+                                              (sequence (comp
+                                                           (distinct)
+                                                           (map (fn[v] {:value v
+                                                                        :from (.indexOf values v)
+                                                                        :to (.lastIndexOf values v)})))
+                                                        values))]) permutation))
+
+        rrt (let [perm  (into {} (map (fn[[k v]] [k (map second v)]) permutation))
+                  zigzagTo (fn [[a b]] (mapv (fn[recnr] (.indexOf b recnr)) a))
+                  fromPerm (mapv (fn[x](get perm x)) keyorder)
                   toPerm (conj (vec (rest fromPerm)) (first fromPerm))
                   mergePerms (fn [m from to]
                                (if (empty? from)
@@ -73,43 +81,14 @@
                                    (recur (conj m [ffrom fto]) (rest from) (rest to)))))
                   pre-consist-rrt (into [] (comp (map zigzagTo) ) (mergePerms [] fromPerm toPerm))]
               (map (fn[column](let[columnName (nth keyorder (.indexOf pre-consist-rrt column))]
-                                (map (fn[cell] [(let[valueColumn (get fvt columnName)]
+                                (map (fn[cell] [(let[valueColumn (get fvt columnName)
+                                                     index (.indexOf column cell)]
                                                   (.indexOf valueColumn
-                                                           (first (filter (fn [x] (let [index (.indexOf column cell)](and (<= index (:to x)) (>= index (:from x))))) valueColumn))
-                                                            )
-                                                  )
+                                                           (first (filter #(and (<= index (:to %)) (>= index (:from %))) valueColumn))))
                                                 cell]) column))) pre-consist-rrt))]
      (Transrelation. keyorder fvt rrt)))
   ([keyorder fvt rrt]
    (Transrelation. keyorder fvt rrt)))
 
-
-
-
-(defn- convert
-  ""
-  [trans-table]
-  (let [ rrt (recordReconst trans-table)
-         recResort (fn [a l result]
-                     (if (empty? l)
-                       result
-                       (let[toSort (first l)
-                            sorted (seq (sort-by (fn[x] (.indexOf a (.indexOf toSort x) )) toSort))]
-                         (recur sorted (rest l) (conj result sorted)))))
-         newRRT (apply conj [(first rrt)]  (recResort (first rrt) (drop-last (rest rrt)) []))
-         recApplySort (fn [values sortVectors result]
-                        (if (empty? values)
-                          result
-                          (recur (rest values)
-                                 (rest sortVectors)
-                                 (conj result (map (fn[x] (nth (first values) x)) (first sortVectors))))))
-         fieldValues (map (fn[k] (get (fieldValues trans-table) k)) (keyorder trans-table))
-         fieldValues (apply conj [(first fieldValues) ] (recApplySort (rest fieldValues) newRRT []))
-         mergeBack (fn [ sortedValues result ]
-                     (if (empty? (first sortedValues))
-                         result
-                         (recur (map rest sortedValues) (conj result (map first sortedValues)))))
-         restructruedValues (mergeBack fieldValues [])]
-    (vec (map (fn[tuple] (zipmap (keyorder trans-table) tuple)) restructruedValues))))
 
 

27c9989658b52e036cb725e3abcf1f18d505b8c8
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2015-11-24 20:00:52 +0100

    renamed project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject more.relation "0.1.0-SNAPSHOT"
+(defproject more.relational "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"

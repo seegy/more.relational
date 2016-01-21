9a15099a0dd747bd63f4e5e70dbd84da91b37b59
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2016-01-21 16:26:54 +0100

    create some tests for hashrel

--- test/relation/hashRel_tests.clj
+++ test/relation/hashRel_tests.clj
@@ -39,19 +39,17 @@
 (deftest rename-test
   (testing "rename"
     (let [renamed (rename people {:sno :id, :scity :town})]
-      (do
         (is (not-any? #(contains? % :sno) (set renamed)))
         (is (not-any? #(contains? % :scity) (set renamed)))
         (is (every? #(contains? % :id) (set renamed)))
         (is (every? #(contains? % :town) (set renamed)))
-        (is (= (count renamed) (count people))))))
+        (is (= (count renamed) (count people)))))
 
   (testing "rename*"
     (let [renamed (rename* r #"(.+)" "prefix-$1")]
-      (do
         (is (every? (fn[attrs] (.startsWith (name attrs) "prefix-")) (scheme renamed)))
         (is (every? (fn[tuple] (every? #(.startsWith (name (first %)) "prefix-") tuple)) renamed))
-        (is (= (count renamed) (count r)))))))
+        (is (= (count renamed) (count r))))))
 
 
 
@@ -63,34 +61,31 @@
   (testing "restrict"
     (let [pred #(and (= 10 (:status %)) (= "Paris" (:scity %)))
           restricted (restrict people pred)]
-      (do
         (is (every? pred (set restricted) ))
         (is (not-any? #(contains? (set restricted) %)
-                      (set (restrict people #(not (pred %))))))))))
+                      (set (restrict people #(not (pred %)))))))))
 
 
 
 (deftest project-test
   (testing  "project"
     (let [projected (project people [:sno :status])]
-      (do
         (is (= (set (scheme projected))))
         (is (not-any? #(contains? % :sname) (set projected)))
         (is (not-any? #(contains? % :scity) (set projected)))
         (is (every? #(contains? % :sno) (set projected)))
         (is (every? #(contains? % :status) (set projected)))
-        (is (= (count projected) (count people))))))
+        (is (= (count projected) (count people)))))
+
   (testing "project+"
     (let [projected (project+ r {:id*2 (relfn [t] (* 2 (:id t)))})]
-      (do
         (is (contains? (set (scheme projected)) :id*2))
         (is (every? #(= (:id*2 %) (* 2 (:id %))) (set projected)))
-        (is (= (count projected) (count r)))))
+        (is (= (count projected) (count r))))
     (let [projected (project+ r {:new "new"})]
-      (do
         (is (contains? (set (scheme projected)) :new))
         (is (every? #(= "new" (:new %)) (set projected)))
-        (is (= (count projected) (count r)))))))
+        (is (= (count projected) (count r))))))
 
 
 
@@ -98,40 +93,63 @@
 (deftest join-test
   (testing "join"
     (let [r-rs (join r rs)]
-      (do
         (is (= #{:id :name :address :sid :quantity} (set (scheme r-rs))))
         (is (every? #(= #{:id :name :address :sid :quantity} (set (keys %))) (set r-rs)))
-        (is (every? (fn[r-tuple]
+        (is (every? (fn [r-tuple]
                       (some (fn[r-rs-tuple]
                                (every?
-                                #(contains? (set (vals r-rs-tuple)) %)) (vals r-tuple)) (seq r-rs)))  (seq r)))))))
+                                #(contains? (set (vals r-rs-tuple)) %) (vals r-tuple))) (seq r-rs)))  (seq r))))
+    (let [r-rs-s (join r (join rs s))]
+        (is (= #{:id :name :address :sid :quantity :description} (set (scheme r-rs-s))))
+        (is (every? #(= #{:id :name :address :sid :quantity :description} (set (keys %))) (set r-rs-s)))
+        (is (every? (fn [r-s-tuple]
+                      (some (fn[r-rs-s-tuple]
+                               (every?
+                                #(contains? (set (vals r-rs-s-tuple)) %) (vals r-s-tuple))) (seq r-rs-s)))  (apply conj (seq r) (seq s))))))
+
+  (testing "compose"
+    (let [composed (compose r rs)]
+        (is (= #{ :name :address :sid :quantity } (set (scheme composed))))
+        (is (every? #(= #{ :name :address :sid :quantity } (set (keys %))) (set composed)))
+        (is (= (set composed ) (set (project- (join r rs) #{:id})))))))
 
 
 
+(deftest set-operators-test
+  (testing "union"
+    (let [ xrel  #{ {:id 1, :name "Arthur", :address "somewhere"}
+                    {:id 11, :name "xxxxArthur", :address "xxxxsomewhere"}
+                    {:id 12, :name "xxxxBetty" :address "xxxxnowhere"} }
+          unioned (union r (rel xrel))]
+        (is (every? #(contains? (set unioned) %) xrel))
+        (is (every? #(contains? (set unioned) %) (set r)))
+      (is (every? #(or (contains? xrel %) (contains? (set r) %)) unioned))))
+
+  (testing "intersect"
+    (let [xrel  #{ {:id 1, :name "Arthur", :address "somewhere"}
+                    {:id 11, :name "xxxxArthur", :address "xxxxsomewhere"}
+                    {:id 12, :name "xxxxBetty" :address "xxxxnowhere"} }
+          intersected (intersect r (rel xrel))]
+      (is (every? #(and (contains? xrel %) (contains? (set r) %)) intersected))))
+
+  (testing "difference"
+    (let [xrel  #{ {:id 1, :name "Arthur", :address "somewhere"}
+                    {:id 11, :name "xxxxArthur", :address "xxxxsomewhere"}
+                    {:id 12, :name "xxxxBetty" :address "xxxxnowhere"} }
+          differenced1 (difference r (rel xrel))
+          differenced2 (difference (rel xrel) r)]
+      (is (every? #(and (contains? xrel %) (not (contains? (set r) %))) differenced2))
+      (is (every? #(and (not (contains? xrel %)) (contains? (set r) %)) differenced1)))))
 
 
+
+(deftest devide-test
+  (testing "devide"
+    (let [])))
 
 #_(
 
 
-(join r rs)
-
-(join (join r rs) s)
-
-(compose r rs)
-
-(compose (compose r rs) s)
-
-(def r2 (rel #{ {:id 1, :name "Arthur", :address "somewhere"} {:id 11, :name "xxxxArthur", :address "xxxxsomewhere"} {:id 12, :name "xxxxBetty" :address "xxxxnowhere"} }))
-
-(union r r2)
-(union r2 r)
-
-
-(intersect r r2)
-
-(difference r r2)
-(difference r2 r)
 
 
 (divide r (project r #{:address}))

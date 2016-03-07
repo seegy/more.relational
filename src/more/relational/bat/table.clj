4fc961905ebbd00cd615f68e4eee8c0f30b0cfc4
Sören Gutzeit <soeren.gutzeit@mni.thm.de>
2016-03-07 21:00:12 +0100

    increase converttobat

--- src/more/relational/bat/table.clj
+++ src/more/relational/bat/table.clj
@@ -11,17 +11,17 @@
 
   clojure.lang.Seqable
   (seq [this]
-      ; just make a sequence
-      (seq (.buns this)))
+       ; just make a sequence
+       (seq (.buns this)))
 
   clojure.lang.Counted
   (count [this]
-    (count (.buns this)))
+         (count (.buns this)))
 
   Object
   (hashCode [this]
-    (let [hashs (conj (map hash (.buns this)) 17)]
-      (apply hashCalcHelper  hashs))))
+            (let [hashs (conj (map hash (.buns this)) 17)]
+              (apply hashCalcHelper  hashs))))
 
 
 
@@ -30,9 +30,9 @@
   ([tuple-vec]
    (if (coll? tuple-vec)
      (let [data (into #{} (comp
-                          (filter #( or (= (keys %) '(:head :tail)) (= (keys %) '(:tail :head))))
-                           (filter map?))
-                       tuple-vec)]
+                            (filter #( or (= (keys %) '(:head :tail)) (= (keys %) '(:tail :head))))
+                            (filter map?))
+                      tuple-vec)]
        (BAT. data))
      (BAT. #{})))
   ([one & more]
@@ -51,40 +51,40 @@
 (defmethod print-method BAT
   [rel writer]
   (.write writer (apply str "#BAT \n"
-                      (map #(str (:head %) " - " (:tail %) "\n") (buns rel)))))
+                        (map #(str (:head %) " - " (:tail %) "\n") (buns rel)))))
 
 
 
 (defn bat?
   [x]
- (= (type x) more.relational.bat.table.BAT))
+  (= (type x) more.relational.bat.table.BAT))
 
 
 
 
 (defn makeTable
   ([orderseq keySeq columns]
-    (let [colums (vec columns)
-          idsOfFirst (vec (map (fn [bun] (dissoc bun :tail)) (first columns)))
-          recurMakeTable (fn [table keySeq columns]
-                            (if (or (empty? keySeq)
-                                    (empty? columns))
-                              (map (fn [row](dissoc row :head)) table)
-                              (let [firstKey (first keySeq)
-                                    firstColumn (map (fn[bun](clojure.set/rename-keys bun {:tail firstKey})) (first columns))
-                                    ks #{:head}
-                                    heads (clojure.set/index table ks)
-                                    newTable (reduce (fn [ret x]
-                                       (let [found (heads (select-keys x ks))]
-                                         (if found
-                                           (reduce #(conj %1 (merge %2 x)) ret found)
-                                           ret)))
-                                     [] firstColumn)]
-                                (recur newTable (drop 1 keySeq) (drop 1 columns)))))
-          orderseq (if (empty? orderseq)
-                     keySeq
-                     orderseq)]
-      (sort-by (apply juxt orderseq )(recurMakeTable idsOfFirst keySeq columns))))
+   (let [colums (vec columns)
+         idsOfFirst (vec (map (fn [bun] (dissoc bun :tail)) (first columns)))
+         recurMakeTable (fn [table keySeq columns]
+                          (if (or (empty? keySeq)
+                                  (empty? columns))
+                            (map (fn [row](dissoc row :head)) table)
+                            (let [firstKey (first keySeq)
+                                  firstColumn (map (fn[bun](clojure.set/rename-keys bun {:tail firstKey})) (first columns))
+                                  ks #{:head}
+                                  heads (clojure.set/index table ks)
+                                  newTable (reduce (fn [ret x]
+                                                     (let [found (heads (select-keys x ks))]
+                                                       (if found
+                                                         (reduce #(conj %1 (merge %2 x)) ret found)
+                                                         ret)))
+                                                   [] firstColumn)]
+                              (recur newTable (drop 1 keySeq) (drop 1 columns)))))
+         orderseq (if (empty? orderseq)
+                    keySeq
+                    orderseq)]
+     (sort-by (apply juxt orderseq )(recurMakeTable idsOfFirst keySeq columns))))
   ([orderSeq keySeq one & moreColumns]
    (makeTable orderSeq keySeq (conj (seq moreColumns) one))))
 
@@ -93,13 +93,31 @@
 (defn convertToBats
   ""
   ([table]
-  (let [heads (reduce (fn [heads entry](apply conj heads (keys entry))) #{} table)]
+   (let [heads (reduce (fn [heads entry](apply conj heads (keys entry))) #{} table)]
      (convertToBats heads table)))
   ([heads table]
-   (let [table (vec table)]
-     (reduce (fn [m attr]
-               (assoc m attr (bat
-                               (filter #(not (nil? (:tail %)))
-                                      (map-indexed (fn [i entry] {:head i,
-                                                                  :tail (get entry attr)}) table))))) {} heads))))
+   (let [head-attr (loop [head-attr :head]
+                     (if (contains? (set heads) head-attr)
+                       (recur (keyword (gensym "head")))
+                       head-attr))
+
+         table (map-indexed (fn[i t] (assoc t head-attr i)) table)]
+     (into {} (map (fn [ k]
+                     [k (BAT. (clojure.set/rename
+                                (into #{} (map #(select-keys % [head-attr k]) table))
+                                (if (= head-attr :head)
+                                  {k :tail}
+                                  {k :tail, head-attr :head})))])
+                   heads)))))
+
+
+
+
+
+
+
+
+
+
+
 
